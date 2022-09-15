ff9557e871d497861cb9384b33ae1cbc98c580c9
KMS\trangpham <trangpham@kms-technlogy.com>
2022-09-15 21:36:41 +0700

    update code

--- script.js
+++ script.js
@@ -1,218 +1,423 @@
 window.onload = () => {
-    tasks.forEach(item => item.state = "show");
-    Task.display();
-}
+  tasks.forEach((item) => (item.state = "show"));
+  Task.display();
+};
 
 let tasks = [];
-const getTasks = localStorage.getItem('tasks');
-
+const getTasks = localStorage.getItem("tasks");
 if (getTasks) tasks = JSON.parse(getTasks);
 
-const input = document.getElementById('task'),
-    createBtn = document.getElementById('create-task'),
-    search_btn = document.getElementById('search-task'),
-    refresh = document.getElementById('refresh'),
-    clear__all = document.querySelector('.clear__all');
-    register_text = document.querySelector('.registerClass');
-    login_text = document.querySelector('.loginClass');
-    register_btn = document.querySelector('.signupreal');
-    login_btn = document.querySelector('.signinreal');
+let accounts = [];
+const getAccounts = localStorage.getItem("accounts");
+
+if (getAccounts) accounts = JSON.parse(getAccounts);
+
+const input = document.getElementById("task"),
+  request_demo = document.getElementById("request_demo")
+register_btn = document.getElementById("signupreal"),
+  register_text = document.getElementById("registerClass"),
+  createBtn = document.getElementById("create-task"),
+  search_btn = document.getElementById("search-task"),
+  refresh = document.getElementById("refresh"),
+  clear__all = document.querySelector(".clear__all");
+login_text = document.querySelector(".loginClass");
+login_btn = document.querySelector(".signinreal");
+title = document.getElementById("title");
+filters = document.querySelectorAll(".form-check-input");
+
+
+let isLogined = localStorage.getItem("isLogined") === "true";
+if (isLogined) {
+  login_text.innerHTML = "Logout";
+} else {
+  window.location.href = "login.html";
+}
+
+const getPlant = (plan) => {
+
+  if (plan == "free") return "premium";
+  else return "free";
+};
+
+const getName = localStorage.getItem("name");
+let user = {};
+if (getName) {
+  user = accounts.filter((e) => e.name == getName)[0];
+}
+
+register_text.innerHTML = user.plan;
+
 
 class Task {
-    // display tasks
-    static display() {
-        const tasks_container = document.getElementById('tasks');
-        let _tasks = '';
-        tasks.forEach((task, index) => {
-            _tasks += `                                         
-                <div class="task-item ${task.state === "show" ? 'mt-2 d-flex justify-content-between align-items-center' : 'd-none'}">
+  // display tasks
+  static display() {
+    console.log(tasks);
+    const tasks_container = document.getElementById("tasks");
+    let _tasks = "";
+    tasks.forEach((task, index) => {
+      _tasks += ` 
+
+
+                <div class="action btns ${
+                  task.state === "show"
+                    ? "mt-2 d-flex justify-content-between align-items-center"
+                    : "d-none"
+                } ${
+        task.completed === "true" ? "text-decoration-line-through" : "text-dark"
+      }">
+                    ${task.title}
+                    <br/>
+                    create at: ${this.unixTimeToHumanReadable(task.create_at)} 
+                    update at: ${this.unixTimeToHumanReadable(task.update_at)}
+                    <button type="button" class="btn btn-sm btn-success is__completed" onclick="Task.todoCompleted('${
+                      task.id
+                    }')"><i class="fa-solid fa-circle-check"></i></button>
+                    <button type="button" class="btn btn-sm btn-primary edit" onclick="Task.update('${
+                      task.id
+                    }')"><i class="fa-solid fa-pen-to-square"></i></button>
+                    <button type="button" class="btn btn-sm btn-danger ms-1 delete" onclick="Task.delete('${
+                      task.id
+                    }')"><i class="fa-solid fa-trash-can"></i></button>
+                </div> 
+
+                <div class="task-item ${
+                  task.state === "show"
+                    ? "mt-2 d-flex justify-content-between align-items-center"
+                    : "d-none"
+                }">
                     <div class="task-name">
-                        <p class="${task.completed === 'true' ? 'text-decoration-line-through' : 'text-dark'}" id="task__name">${task.name}</p>
-                    </div>
-                    <div class="action btns">
-                        <button type="button" class="btn btn-sm btn-success is__completed" onclick="Task.todoCompleted('${task.id}')"><i class="fa-solid fa-circle-check"></i></button>
-                        <button type="button" class="btn btn-sm btn-primary edit" onclick="Task.update('${task.id}')"><i class="fa-solid fa-pen-to-square"></i></button>
-                        <button type="button" class="btn btn-sm btn-danger ms-1 delete" onclick="Task.delete('${task.id}')"><i class="fa-solid fa-trash-can"></i></button>
+                        <p  id="task__name">${task.name}</p>
                     </div>
                 </div>
+                <br/>
             `;
-        });
-        (tasks.length === 0 || _tasks === '') ? clear__all.classList.add('d-none'): clear__all.classList.remove('d-none');
-        tasks_container.innerHTML = _tasks;
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
+    });
+    tasks.length === 0 || _tasks === "" ?
+      clear__all.classList.add("d-none") :
+      clear__all.classList.remove("d-none");
+    tasks_container.innerHTML = _tasks;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 
-    // create task
-    static create(task) {
-        const generateRandomId = Math.floor(Math.random() * 99999);
-        tasks.push({
-            id: generateRandomId,
-            name: task,
-            completed: 'false',
-            state: 'show'
-        });
-        this.display();
-    }
+  // create task
+  static create(task, title) {
+    const generateRandomId = Math.floor(Math.random() * 99999);
+    tasks.push({
+      id: generateRandomId,
+      name: task,
+      title: title,
+      completed: "false",
+      state: "show",
+      create_at: Math.round(+new Date() / 1000),
+      update_at: Math.round(+new Date() / 1000),
+    });
+    this.display();
+  }
 
-    // completed
-    static todoCompleted(task) {
-        tasks.forEach(item => {
-            if (`${item.id}` === task) {
-                if (item.completed === 'false')
-                    item.completed = 'true';
-                else
-                    item.completed = 'false';
-            }
-        });
+  // completed
+  static todoCompleted(task) {
+    tasks.forEach((item) => {
+      if (`${item.id}` === task) {
+        if (item.completed === "false") item.completed = "true";
+        else item.completed = "false";
+      }
+    });
 
-        this.display();
-    }
+    this.display();
+  }
 
-    // update/edit task
-    static update(task) {
-        const taskItems = document.querySelectorAll('.task-item');
-        const taskInput = document.getElementById('task-input');
-        const edit = document.querySelectorAll('.task-name');
+  // update/edit task
+  static update(task) {
+    const taskItems = document.querySelectorAll(".task-item");
+    const taskInput = document.getElementById("task-input");
+    const edit = document.querySelectorAll(".task-name");
 
-        tasks.forEach((item, index) => {
-            if (`${item.id}` === task) {
-                taskItems[index].classList.add('task-editing');
-                edit[index].innerHTML = `
-                    <input type="text" id="task-input" class="form-control" value="${item.name}" placeholder="Edit this Todo and Hit Enter!" title="Edit this Todo and Hit Enter!" />
-                    <button type="button" class="btn btn-primary save-edited-todo">Edit This</button>
+    tasks.forEach((item, index) => {
+      if (`${item.id}` === task) {
+        taskItems[index].classList.add("task-editing");
+        edit[index].innerHTML = `
+                    <input type="text" id="task-title-${task}" class="form-control" value="${item.title}" placeholder="Edit this Todo and Hit Enter!" title="Edit this Todo and Hit Enter!" />
+                
+                    <textarea id="task-name-${task}" style="margin-top:5px" >${item.name}</textarea>
+                        
+                    <button id="save-${task}" type="button" class="btn btn-primary">Edit This</button>
                 `;
 
-                const taskInputs = document.querySelectorAll('#task-input');
-                const saveEditTodo = document.querySelector('.save-edited-todo');
-                if (taskInputs) {
-                    taskInputs.forEach(input => {
-                        input.addEventListener('keydown', e => {
-                            if (e.key === 'Enter') {
-                                if (input.value === '') showError('.error', 'Edit Field Cannot be Empty!');
+        const taskInputs = document.getElementById(`task-name-${task}`);
+        const titleInputs = document.getElementById(`task-title-${task}`);
 
-                                saveEditTodo.addEventListener('click', e => {
-                                    let input_value = input.value;
-                                    if (input_value) this.update(task, input_value);
-                                });
+        const saveEditTodo = document.getElementById(`save-${task}`);
 
-                                saveEditTodo.click();
-                            }
-                        });
-                    });
-                }
-
-                if (taskInput.value === '') return;
-
-                item.name = taskInput.value;
+        if (taskInputs && titleInputs) {
+          saveEditTodo.addEventListener("click", (e) => {
+            let input_value = taskInputs.value;
+            let title_value = titleInputs.value;
+            if (input_value && title_value) {
+              item.name = input_value;
+              item.title = title_value;
+              item.update_at = Math.round(+new Date() / 1000);
+              this.display();
+            } else {
+              showError(".error", "Edit Field Cannot be Empty!");
             }
-        });
+          });
+        }
+      }
+    });
+  }
 
-        this.display();
+  // delete task
+  static delete(task) {
+    tasks = tasks.filter((item) => `${item.id}` !== task);
+
+    // tasks.forEach((item, index) => {
+    //     if(`${item.id}` === task) {
+    //         tasks.splice(index, 1)
+    //     }
+    // });
+    this.display();
+  }
+
+  // search task
+  static search(task) {
+
+    tasks = tasks.filter((item) =>
+      item.title.toLowerCase() === task.toLowerCase() ?
+      (item.state = "show") :
+      (item.state = "hide")
+    );
+
+    const checkTask = (element) =>
+      element.title.toLowerCase() === task.toLowerCase();
+    if (tasks.some(checkTask) === false) {
+      tasks.forEach((item) => {
+        item.state = "hide"
+      });
+    }
+    this.display();
+  }
+
+  static unixTimeToHumanReadable(seconds) {
+    seconds = seconds + 7 * 60 * 60;
+    // Save the time in Human
+    // readable format
+    let ans = "";
+
+    // Number of days in month
+    // in normal year
+    let daysOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    let currYear,
+      daysTillNow,
+      extraTime,
+      extraDays,
+      index,
+      date,
+      month,
+      hours,
+      minutes,
+      secondss,
+      flag = 0;
+
+    // Calculate total days unix time T
+    daysTillNow = parseInt(seconds / (24 * 60 * 60), 10);
+    extraTime = seconds % (24 * 60 * 60);
+    currYear = 1970;
+
+    // Calculating current year
+    while (daysTillNow >= 365) {
+      if (currYear % 400 == 0 || (currYear % 4 == 0 && currYear % 100 != 0)) {
+        daysTillNow -= 366;
+      } else {
+        daysTillNow -= 365;
+      }
+      currYear += 1;
     }
 
-    // delete task
-    static delete(task) {
-        tasks = tasks.filter(item => `${item.id}` !== task);
+    // Updating extradays because it
+    // will give days till previous day
+    // and we have include current day
+    extraDays = daysTillNow + 1;
 
-        // tasks.forEach((item, index) => {
-        //     if(`${item.id}` === task) {
-        //         tasks.splice(index, 1)
-        //     }
-        // });
-        this.display();
+    if (currYear % 400 == 0 || (currYear % 4 == 0 && currYear % 100 != 0))
+      flag = 1;
+
+    // Calculating MONTH and DATE
+    month = 0;
+    index = 0;
+    if (flag == 1) {
+      while (true) {
+        if (index == 1) {
+          if (extraDays - 29 < 0) break;
+
+          month += 1;
+          extraDays -= 29;
+        } else {
+          if (extraDays - daysOfMonth[index] < 0) {
+            break;
+          }
+          month += 1;
+          extraDays -= daysOfMonth[index];
+        }
+        index += 1;
+      }
+    } else {
+      while (true) {
+        if (extraDays - daysOfMonth[index] < 0) {
+          break;
+        }
+        month += 1;
+        extraDays -= daysOfMonth[index];
+        index += 1;
+      }
     }
 
-    // search task
-    static search(task) {
-        tasks = tasks.filter(item => item.name.toLowerCase() === task ? item.state = "show" : item.state = "hide");
-
-        const checkTask = element => element.name.toLowerCase() === `${task.toString()}`;
-        if (tasks.some(checkTask) === false) {
-            showError('.error', 'Todo, Does not Exists!');
-            return clear__all.classList.add('d-none');
-        } else clear__all.classList.remove('d-none');
-
-        this.display();
+    // Current Month
+    if (extraDays > 0) {
+      month += 1;
+      date = extraDays;
+    } else {
+      if (month == 2 && flag == 1) date = 29;
+      else {
+        date = daysOfMonth[month - 1];
+      }
     }
+
+    // Calculating HH:MM:YYYY
+    hours = parseInt(extraTime / 3600, 10);
+    minutes = parseInt((extraTime % 3600) / 60, 10);
+    secondss = parseInt((extraTime % 3600) % 60, 10);
+
+    ans += date.toString();
+    ans += "/";
+    ans += month.toString();
+    ans += "/";
+    ans += currYear.toString();
+    ans += " ";
+    ans += hours.toString();
+    ans += ":";
+    ans += minutes.toString();
+    ans += ":";
+    ans += secondss.toString();
+
+    // Return the time
+    return ans;
+  }
 }
 
 // Create Btn
-createBtn.addEventListener('click', (e) => {
-    const input_value = input.value;
-    if (input_value !== '') {
-        input.value = '';
-        Task.create(input_value);
-    } else {
-        showError('.error', 'Cannot Add Todo!');
-    }
+createBtn.addEventListener("click", (e) => {
+  const input_value = input.value;
+  const input_title = title.value;
+  if (input_value !== "" && input_title !== "") {
+    input.value = "";
+    title.value = "";
+    Task.create(input_value, input_title);
+  } else {
+    showError(".error", "Cannot Add Todo!");
+  }
 });
 
 // Search Btn
-search_btn.addEventListener('click', e => {
-    let task = input;
-    let search_value = input.value;
+search_btn.addEventListener("click", (e) => {
+  let task = title;
+  let search_value = title.value;
 
-    if (search_value != '') {
-        task.style.border = '1px solid gray';
-        input.value = '';
-        Task.search(search_value.toLowerCase());
-    } else {
-        showError('.error', 'Search Field Cannot be Empty!');
-        task.style.border = '1px solid red';
-    }
+  if (search_value != "") {
+    task.style.border = "1px solid gray";
+    input.value = "";
+    Task.search(search_value.toLowerCase());
+  } else {
+    showError(".error", "Search Field Cannot be Empty!");
+    task.style.border = "1px solid red";
+  }
 });
 
 // Prevent from Submit-ing the Form
-let form = document.querySelector('.form');
-form.addEventListener('submit', e => {
-    e.preventDefault();
+let form = document.querySelector(".form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 });
 
 // Keyboard Support Enter Key (To add a Task) and > Right Arrow (to Search)
-input.addEventListener('keydown', e => {
-    if (e.key === 'Enter') createBtn.click();
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") createBtn.click();
 
-    if (e.key === 'ArrowRight') search_btn.click();
+  if (e.key === "ArrowRight") search_btn.click();
 });
 
 // Errors Function
 function showError(error_place, error_message) {
-    const error_container = document.querySelector(error_place);
-    if (error_container) {
-        error_container.innerHTML = `
+  const error_container = document.querySelector(error_place);
+  if (error_container) {
+    error_container.innerHTML = `
             <div class="alert alert-danger error" role="alert">
                 ${error_message}
             </div>
         `;
-        setTimeout(() => error_container.innerHTML = '', 3000);
-    }
+    setTimeout(() => (error_container.innerHTML = ""), 0);
+  }
 }
 
 // Clear All Btn
 function clearAll() {
-    tasks = [];
-    Task.display();
+  tasks = [];
+  Task.display();
 }
 
+clear__all.addEventListener("click", clearAll);
 
-clear__all.addEventListener('click', clearAll);
-
-register_text.addEventListener('click',()=>{
-    alert('Clicked!')
-    setTimeout(function(){
-        window.location.href = "register.html";
-    },333);
-})
-
-login_text.addEventListener('click',()=>{
-    alert('Clicked Login!')
-    setTimeout(function(){
-        window.location.href = "login.html";
-    },333);
-})
+register_text.addEventListener("click", () => {
 
 
+  accounts.map((o) => {
+    if (o.name == user.name) {
+
+      o.plan = getPlant(o.plan)
+    }
+
+  });
+  localStorage.setItem("accounts", JSON.stringify(accounts))
+  location.reload();
+
+});
+
+login_text.addEventListener("click", () => {
+  // alert("Clicked Login!");
+  setTimeout(function () {
+    window.location.href = "login.html";
+  }, 0);
+});
+
+filters.forEach((item) => {
+  item.addEventListener("change", (event) => {
+    console.log(item.id);
+    if (item.id == "completed") {
+      tasks = tasks.filter((item) =>
+        item.completed.toLowerCase() != "false" ?
+        (item.state = "show") :
+        (item.state = "hide")
+      );
+    } else if (item.id == "uncompleted") {
+      tasks = tasks.filter((item) =>
+        item.completed.toLowerCase() == "false" ?
+        (item.state = "show") :
+        (item.state = "hide")
+      );
+    } else {
+      tasks = tasks.filter((item) => (item.state = "show"));
+    }
+    Task.display();
+  });
+});
 
 
 // Refresh Page
-refresh.addEventListener('click', () => location.href = location.href);
+refresh.addEventListener("click", (e) => {
+  location.href = location.href
+});
+
+
+request_demo.addEventListener("click", (e) => {
+  window.location.href = "request.html";
+})
